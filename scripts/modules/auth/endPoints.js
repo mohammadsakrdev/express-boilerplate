93f4a5dd7dcc6ab3743b1aa09b3e5e36c57f6693
Mohammad Sakr <mohammad_sakr_89@yahoo.com>
2020-04-29 11:12:52 +0200

    Remove unneeded files

--- scripts/modules/auth/endPoints.js
+++
@@ -1,8 +0,0 @@
-const authEndPoints = Object.freeze({
-  AUTH_EMAIL_VERIFICATION: 'auth:emailVerification',
-  AUTH_EMAIL_CONFIRMATION: 'auth:emailConfirmation',
-  AUTH_PHONE_VERIFICATION: 'auth:phoneVerification',
-  AUTH_PHONE_CONFIRMATION: 'auth:phoneConfirmation'
-});
-
-module.exports = authEndPoints;
